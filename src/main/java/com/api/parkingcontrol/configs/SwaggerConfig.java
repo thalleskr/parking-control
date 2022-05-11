5a4180b4459083c7ebeec86e099bca8c4a9a246a
Thalles <thallicuto3@gmail.com>
2022-05-11 03:55:27 -0300

    Initial commit

---
+++ src/main/java/com/api/parkingcontrol/configs/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.api.parkingcontrol.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.api.parkingcontrol"))
+                .paths(regex("/parking-spot.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Parking Spot",
+                "API para o cadastro de Vagas de estacionamento, para um complexo de condominio.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Thalles Rangel",
+                        "https://github.com/thalleskr",
+                        "thallicuto3@gmail.com"),
+                "#",
+                "#", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+
+}
